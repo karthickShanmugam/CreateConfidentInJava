ad6991625190a0e00a6c6869ad25a2d89f26062b
karthick2011.sha <karthick2011.sha@gmail.com>
2025-10-26 13:23:00 +0530

    upcasting and downcasting

--- src/com/cmj/DynamicDispatchTest.java
+++ src/com/cmj/DynamicDispatchTest.java
@@ -3,11 +3,18 @@ class A {
     public void show(){
         System.out.println("in A show");
     }
+
+    public void showA(){
+        System.out.println("in AA show");
+    }
 }
 
 class B extends A {
     public void show(){
         System.out.println("in B show");
+    }
+    public void showB(){
+        System.out.println("in BB show");
     }
 }
 
@@ -26,6 +33,10 @@ public class DynamicDispatchTest {
         a.show();
         a = new B();
         a.show();
+        a.showA();
+        B bb = (B)a;
+        bb.showB();
+
         a= new C();
         a.show();
 
--- src/com/cmj/UpCasting_DownCasting.java
+++ src/com/cmj/UpCasting_DownCasting.java
@@ -1,4 +1,35 @@
 package com.cmj;
 
-public class UpCasting_DownCasting {
+class CastingA{
+    public void show(){
+        System.out.println("showA");
+    }
+
+    public void showAA(){
+        System.out.println("showAA");
+    }
 }
+
+class CastingB extends CastingA{
+    public void show(){
+        System.out.println("showB");
+    }
+
+    public void showBB(){
+        System.out.println("showBB");
+    }
+}
+
+public class UpCasting_DownCasting {
+    public static void main(String[] args) {
+       CastingA a = new CastingB(); // Upcasting
+        a.show();
+        a.showAA();
+
+        CastingB b = (CastingB) a;  //down casting
+        b.show();
+        b.showBB();
+        b.showAA();
+    }
+}
+
