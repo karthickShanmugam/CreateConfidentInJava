85e9ef7a4eed27fb0c9799f61e12f4707d1ae072
Prakash <prakashyadu29@gmail.com>
2025-11-11 09:35:12 +0530

    # java8 Question : start with 1 , even  and Odd num in list partition, patition by palindrom

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+/*void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
@@ -10,4 +10,4 @@ void main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
     }
-}
+}*/
--- src/com/cmj/yaduInterviewQ/java8/java8Test.java
+++ src/com/cmj/yaduInterviewQ/java8/java8Test.java
@@ -14,14 +14,14 @@ public class java8Test {
         String[] stringarry =strInput.split(" ");
         // print even
         var even = myList.stream().filter(env->env%2==0).toList();
-                System.out.println("even:" +even);
+        System.out.println("even:" +even);
         // print even and old
         var evenAndOdd = myList.stream().collect(Collectors.partitioningBy(env->env%2==0));
         System.out.println("even:" +evenAndOdd);
 
         //Find the repetitive character from a word or sentence using java8
 
-       var repetitiveSeqChar = strInput.chars().filter(c -> !Character.isWhitespace(c)).mapToObj(c->(char)c).
+        var repetitiveSeqChar = strInput.chars().filter(c -> !Character.isWhitespace(c)).mapToObj(c->(char)c).
                 collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).entrySet().stream().filter(entry->entry.getValue()>1).collect(Collectors.toList());
         System.out.println("repetitive character from a word:" +repetitiveSeqChar);
 
@@ -39,17 +39,69 @@ public class java8Test {
 
         // Employee max salary by department
         List<Employee> employeeList = Arrays.asList(
-        new Employee("ram","male","IT",110000.00),
-       new Employee("jam","female","HR",40000.00),
-       new Employee("jaua","male","LT",10000.00),
-         new Employee("jaya","female","IT",4000000.00)
-    );
+                new Employee("ram","male","IT",110000.00),
+                new Employee("jam","female","HR",40000.00),
+                new Employee("jaua","male","LT",10000.00),
+                new Employee("jaya","female","IT",4000000.00)
+        );
 
 
         var emplist =employeeList.stream().
-        collect(Collectors.
-                groupingBy(Employee ::getDepartment,Collectors.maxBy(Comparator.comparing(Employee::getSalary))));
+                collect(Collectors.
+                        groupingBy(Employee ::getDepartment,Collectors.maxBy(Comparator.comparing(Employee::getSalary))));
         System.out.println("Employee max salary by department"+emplist);
+
+        //// print duplicate from list
+        List<Integer> ipNum = Arrays.asList(1,2,2,5,7,8,7);
+        System.out.println("Duplicate i/p"+ipNum);
+
+        HashSet<Integer> notAllowDuplicate = new HashSet<>();
+
+        var duplicateNum = ipNum.stream().
+                filter(n->!notAllowDuplicate.add(n)).
+                collect(Collectors.toSet());
+        System.out.println("w/o Duplicate o/p"+duplicateNum);
+
+        //// start with A and salry > 1000
+
+        var result = employeeList.stream()
+                .filter(e -> e.getName().startsWith("r"))
+                .filter(e -> e.getSalary() > 1000)
+                .collect(Collectors.toList());
+        System.out.println("start with A and salry > 1000"+result);
+
+        // duplicate word in String array
+        String[] arr = {
+                "java Ruby Struts",
+                "spring java",
+                "spring python spring"
+        };
+
+        Map<String, Long> wordCount =
+                Arrays.stream(arr)
+                        .flatMap(s -> Arrays.stream(s.toLowerCase().split("\\s+")))
+                        .collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+        wordCount.forEach((k, v) -> System.out.println(k + " = " + v));
+
+        /// patition by palindrom
+        List<Integer> numbers = Arrays.asList(121, 23, 11, 454, 67, 101, 22, 900);
+
+        var  listPalindrom = numbers.stream()
+                .collect(Collectors.partitioningBy(n->n.toString().equals( new StringBuilder(n.toString()).reverse().toString()      )));
+        System.out.println("Palindrom"+listPalindrom);
+
+        // from list find start with 1
+
+        var startWithOne = numbers.stream().filter(n->n.toString().startsWith("1")).collect(Collectors.toList());
+
+        System.out.println("start with 1 "+startWithOne);
+
+        // even numbers and odd number in List
+        var evenAndOddNums = numbers.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
+        System.out.println("even  and Odd num in list : "+evenAndOddNums);
+
+
 
 
 
